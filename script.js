d41ce08d0ff78ef19efc9b88bc1da8a9c498e390
Tomas 'Yomi' Caraballo <yomizana@gmail.com>
2023-03-02 11:42:48 -0400

    Render contents of the gameboard array to the webpage

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 /* eslint-disable no-console */
+
+const GameBoard = (() => {
+  const gameBoardArray = ["X", "X", "X", "X", "X", "X", "X", "X", "X"];
+  return { gameBoardArray };
+})();
+
+const Player = () => {
+  // Do stuff.
+};
+
+(function renderContents() {
+  const fields = document.querySelectorAll(".field");
+
+  fields.forEach((field, index) => {
+    const position = field;
+    position.textContent = GameBoard.gameBoardArray[index];
+  });
+})();
