b88ef27da07df89f6d8030d0fc2d7cc167ac0ce9
Tomas 'Yomi' Caraballo <yomizana@gmail.com>
2023-03-03 09:42:38 -0400

    Implement function to generate the gameboard

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 /* eslint-disable no-console */
 
-const GameBoard = (() => {
-  const gameBoardArray = ["X", "X", "X", "X", "X", "X", "X", "X", "X"];
-  return { gameBoardArray };
-})();
+function Cell() {
+  const value = 0;
 
-const Player = () => {
-  // Do stuff.
-};
+  const getValue = () => value;
 
-(function renderContents() {
-  const fields = document.querySelectorAll(".field");
+  return { getValue };
+}
 
-  fields.forEach((field, index) => {
-    const position = field;
-    position.textContent = GameBoard.gameBoardArray[index];
-  });
-})();
+function Gameboard() {
+  const gameboard = [];
+  const rows = 3;
+  const columns = 3;
+
+  // Generate 3x3 array grid.
+  for (let i = 0; i < rows; i += 1) {
+    gameboard[i] = [];
+
+    for (let j = 0; j < columns; j += 1) {
+      gameboard[i].push(Cell());
+    }
+  }
+
+  const displayBoard = () => {
+    const board = gameboard.map((row) => row.map((cell) => cell.getValue()));
+    console.log(board);
+  };
+
+  return { displayBoard };
+}
+
+const game = Gameboard();
+game.displayBoard();
